8b045180d785d56b474527f0fb1549c0cc668f2f
Okasanmi Erickkson Ariyo <87527273+herryckksen@users.noreply.github.com>
2022-06-11 22:57:51 +0100

    update

--- js/custom.js
+++ js/custom.js
@@ -1,30 +1,49 @@
 design.style.display = 'none'
 software.style.display = 'none'
+machine.style.display = 'none'
+
 
 function webVisible(){
     website = document.getElementById('web')
     design = document.getElementById('design')
     software = document.getElementById('software')
+    machine = document.getElementById('machine')
     
     website.style.display = 'block'
     design.style.display = 'none'
     software.style.display = 'none'
+    machine.style.display = 'none'
 }
 function designVisible(){
     website = document.getElementById('web')
     design = document.getElementById('design')
     software = document.getElementById('software')
+    machine = document.getElementById('machine')
     
     website.style.display = 'none'
     design.style.display = 'block'
     software.style.display = 'none'
+    machine.style.display = 'none'
 }
 function softwareVisible(){
     website = document.getElementById('web')
     design = document.getElementById('design')
     software = document.getElementById('software')
+    machine = document.getElementById('machine')
     
     website.style.display = 'none'
     design.style.display = 'none'
     software.style.display = 'block'
+    machine.style.display = 'none'
+}
+function machineVisible(){
+    website = document.getElementById('web')
+    design = document.getElementById('design')
+    software = document.getElementById('software')
+    machine = document.getElementById('machine')
+    
+    website.style.display = 'none'
+    design.style.display = 'none'
+    software.style.display = 'none'
+    machine.style.display = 'block'
 }
